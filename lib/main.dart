ff8536e8937b6e0232fad6fd2c4388a20ee0cb82
gustavo.paes <gustavo910@gmail.com>
2024-08-19 14:43:56 -0300

    refacot: change call screen pricipal for login

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-     //home:  Login()
-        home: const PurchaseItens()
+     home:  Login()
 
     );
   }
